f1eea75fc6d6d0af0bbfa4f2a0cf641e4ec1e34a
UnderTMoon <1569609928@qq.com>
2020-04-05 11:13:33 +0800

    commit;

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login_container">
+      登录组件
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style  scoped>
+  .login_container {
+    background-color: dodgerblue;
+  }
+</style>
