c8b4a57a894db82f8a6519efe90721bcedee3293
UnderTMoon <1569609928@qq.com>
2020-04-08 22:04:19 +0800

    用户列表+分页展示;

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -21,6 +21,46 @@
         </el-col>
       </el-row>
 
+      <!--用户列表区域-->
+      <el-table :data="userList" stripe border>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="姓名" prop="userName"></el-table-column>
+        <el-table-column label="电话" prop="phone"></el-table-column>
+        <el-table-column label="性别" prop="sex"></el-table-column>
+        <el-table-column label="家庭住址" prop="address"></el-table-column>
+        <el-table-column label="身份角色" prop="role"></el-table-column>
+
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state">
+            </el-switch>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="操作" width="180px">
+          <template slot-scope="scope">
+            <!--修改按钮-->
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <!--删除按钮-->
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <!--分配角色按钮-->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!--分页区域-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="queryInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -32,9 +72,13 @@
       return {
         queryInfo: {
           qusey: '',
+          //当前所在页
           pagenum: 1,
-          pageSize: 2
-        }
+          //当前每页显示数据条数
+          pageSize: 10
+        },
+        userList:[],
+        total: 0
       }
     },
     created() {
@@ -44,7 +88,21 @@
       async getUserList() {
          const {data: res} = await this.$http.get('users',{ params: this.queryInfo})
         console.log(res)
-        if(res.msg !== 200) return this.$message.error('获取用户列表失败！')
+        if(res.msg !== 200){
+          return this.$message.error('获取用户列表失败！')
+        }
+        this.userList = res.data.user
+        this.total = res.data.total
+      },
+      //监听pageSize改变事件
+      handleSizeChange(newSize) {
+        this.queryInfo.pageSize = newSize
+        this.getUserList()
+      },
+      //监听页码值改变的事件
+      handleCurrentChange(newPage) {
+        this.queryInfo.pagenum = newPage
+        this.getUserList()
       }
     }
   }
